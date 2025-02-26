5704ffcd77a6a902b612e6c35475abcd0e7d644d
Kishan-Ramani <kishanramani269@gmail.com>
2025-02-26 09:10:23 +0530

    Add RegisterForm with database connection and event handlers
    Updated Program.cs to run RegisterForm instead of Register.
    Added RegisterForm.cs with SQL Server connection, event handlers,
    and data insertion method. Added RegisterForm.Designer.cs with
    form control definitions and layout. Added RegisterForm.resx for
    resource definitions.

---
+++ RegisterForm.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace WinFormsApp1
+{
+    partial class RegisterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            name = new TextBox();
+            Branch = new TextBox();
+            label2 = new Label();
+            Sem = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            citylist = new ListBox();
+            label5 = new Label();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            RegisterButton = new Button();
+            password = new TextBox();
+            label6 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(85, 58);
+            label1.Name = "label1";
+            label1.Size = new Size(48, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Name : ";
+            // 
+            // name
+            // 
+            name.Location = new Point(161, 55);
+            name.Name = "name";
+            name.Size = new Size(223, 23);
+            name.TabIndex = 1;
+            // 
+            // Branch
+            // 
+            Branch.Location = new Point(161, 111);
+            Branch.Name = "Branch";
+            Branch.Size = new Size(223, 23);
+            Branch.TabIndex = 3;
+            Branch.TextChanged += textBox2_TextChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(85, 114);
+            label2.Name = "label2";
+            label2.Size = new Size(53, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Branch : ";
+            // 
+            // Sem
+            // 
+            Sem.Location = new Point(161, 161);
+            Sem.Name = "Sem";
+            Sem.Size = new Size(223, 23);
+            Sem.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(85, 164);
+            label3.Name = "label3";
+            label3.Size = new Size(39, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Sem : ";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(87, 266);
+            label4.Name = "label4";
+            label4.Size = new Size(37, 15);
+            label4.TabIndex = 6;
+            label4.Text = "City : ";
+            label4.Click += label4_Click;
+            // 
+            // citylist
+            // 
+            citylist.FormattingEnabled = true;
+            citylist.ItemHeight = 15;
+            citylist.Items.AddRange(new object[] { "Rajkot", "Morbi", "Ahemdabad" });
+            citylist.Location = new Point(161, 266);
+            citylist.Name = "citylist";
+            citylist.Size = new Size(223, 94);
+            citylist.TabIndex = 7;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(84, 214);
+            label5.Name = "label5";
+            label5.Size = new Size(54, 15);
+            label5.TabIndex = 8;
+            label5.Text = "Gender : ";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(161, 214);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(51, 19);
+            radioButton1.TabIndex = 9;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Male";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(290, 214);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(63, 19);
+            radioButton2.TabIndex = 10;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Female";
+            radioButton2.UseVisualStyleBackColor = true;
+            radioButton2.CheckedChanged += radioButton2_CheckedChanged;
+            // 
+            // RegisterButton
+            // 
+            RegisterButton.Location = new Point(85, 379);
+            RegisterButton.Name = "RegisterButton";
+            RegisterButton.Size = new Size(75, 23);
+            RegisterButton.TabIndex = 11;
+            RegisterButton.Text = "Register";
+            RegisterButton.UseVisualStyleBackColor = true;
+            RegisterButton.Click += button1_Click;
+            // 
+            // password
+            // 
+            password.Location = new Point(476, 55);
+            password.Name = "password";
+            password.Size = new Size(223, 23);
+            password.TabIndex = 13;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(400, 58);
+            label6.Name = "label6";
+            label6.Size = new Size(66, 15);
+            label6.TabIndex = 12;
+            label6.Text = "Password : ";
+            // 
+            // RegisterForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(password);
+            Controls.Add(label6);
+            Controls.Add(RegisterButton);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(label5);
+            Controls.Add(citylist);
+            Controls.Add(label4);
+            Controls.Add(Sem);
+            Controls.Add(label3);
+            Controls.Add(Branch);
+            Controls.Add(label2);
+            Controls.Add(name);
+            Controls.Add(label1);
+            Name = "RegisterForm";
+            Text = "RegisterForm";
+            Load += RegisterForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox name;
+        private TextBox Branch;
+        private Label label2;
+        private TextBox Sem;
+        private Label label3;
+        private Label label4;
+        private ListBox citylist;
+        private Label label5;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private Button RegisterButton;
+        private TextBox password;
+        private Label label6;
+    }
+}
---
+++ RegisterForm.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;    
+
+namespace WinFormsApp1
+{
+    public partial class RegisterForm : Form
+    {
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-6V5I9OV;Initial Catalog=RegisterDB;Integrated Security=True");
+        String gender;
+        public RegisterForm()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RegisterForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String query = "insert into RegisterForm values('" + name.Text + "','" + password.Text + "','" + Branch.Text + "','" + Convert.ToInt32(Sem.Text) + "','"+gender+"','"+citylist.SelectedIndex+"')";
+            SqlCommand cmd = new SqlCommand(query, con);
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Data Inserted Successfully");
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            gender = "MALE";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            gender = "FEMALE";
+        }
+    }
+}
