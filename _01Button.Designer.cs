51b7e452843ef04d91322c2e7595cd9363792561
Kishan-Ramani <kishanramani269@gmail.com>
2025-03-17 13:42:51 +0530

    Revamp 01Button form with database operations
    Complete overhaul of 01Button form design and functionality:
    - Removed old form design and event handlers in `01Button.Designer.cs` and `01Button.cs`
    - Added new form design and event handlers to support database operations
    - Replaced `01Button.resx` with a new version reflecting the new design
    - Changed application startup form in `Program.cs` from `showData` to `_01Button`
    - Implemented database operations (insert, update, delete, display) using `SqlConnection` and `SqlCommand` in `_01Button` class
    - Defined UI components and properties in `_01Button.Designer.cs`
    - Updated resource definitions in the new `_01Button.resx`

---
+++ _01Button.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace WinFormsApp1
+{
+    partial class _01Button
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Insert_BTN = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            name = new TextBox();
+            branch = new TextBox();
+            sem = new TextBox();
+            Update_BTN = new Button();
+            Delete_BTN = new Button();
+            button4 = new Button();
+            dataGridView1 = new DataGridView();
+            ID_Val = new TextBox();
+            ID = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // Insert_BTN
+            // 
+            Insert_BTN.Location = new Point(70, 270);
+            Insert_BTN.Name = "Insert_BTN";
+            Insert_BTN.Size = new Size(68, 36);
+            Insert_BTN.TabIndex = 0;
+            Insert_BTN.Text = "Insert";
+            Insert_BTN.UseVisualStyleBackColor = true;
+            Insert_BTN.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(70, 82);
+            label1.Name = "label1";
+            label1.Size = new Size(39, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Name";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(70, 140);
+            label2.Name = "label2";
+            label2.Size = new Size(44, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Branch";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(70, 210);
+            label3.Name = "label3";
+            label3.Size = new Size(30, 15);
+            label3.TabIndex = 3;
+            label3.Text = "Sem";
+            // 
+            // name
+            // 
+            name.Location = new Point(136, 78);
+            name.Name = "name";
+            name.Size = new Size(193, 23);
+            name.TabIndex = 4;
+            // 
+            // branch
+            // 
+            branch.Location = new Point(136, 140);
+            branch.Name = "branch";
+            branch.Size = new Size(193, 23);
+            branch.TabIndex = 5;
+            // 
+            // sem
+            // 
+            sem.Location = new Point(136, 210);
+            sem.Name = "sem";
+            sem.Size = new Size(193, 23);
+            sem.TabIndex = 6;
+            // 
+            // Update_BTN
+            // 
+            Update_BTN.Location = new Point(144, 270);
+            Update_BTN.Name = "Update_BTN";
+            Update_BTN.Size = new Size(68, 36);
+            Update_BTN.TabIndex = 7;
+            Update_BTN.Text = "Update";
+            Update_BTN.UseVisualStyleBackColor = true;
+            Update_BTN.Click += Update_BTN_Click;
+            // 
+            // Delete_BTN
+            // 
+            Delete_BTN.Location = new Point(218, 270);
+            Delete_BTN.Name = "Delete_BTN";
+            Delete_BTN.Size = new Size(68, 36);
+            Delete_BTN.TabIndex = 8;
+            Delete_BTN.Text = "Delete";
+            Delete_BTN.UseVisualStyleBackColor = true;
+            Delete_BTN.Click += Delete_BTN_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(393, 72);
+            button4.Name = "button4";
+            button4.Size = new Size(287, 48);
+            button4.TabIndex = 9;
+            button4.Text = "Display";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(393, 140);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(342, 214);
+            dataGridView1.TabIndex = 10;
+            // 
+            // ID_Val
+            // 
+            ID_Val.Location = new Point(136, 34);
+            ID_Val.Name = "ID_Val";
+            ID_Val.Size = new Size(193, 23);
+            ID_Val.TabIndex = 12;
+            // 
+            // ID
+            // 
+            ID.AutoSize = true;
+            ID.Location = new Point(70, 38);
+            ID.Name = "ID";
+            ID.Size = new Size(18, 15);
+            ID.TabIndex = 11;
+            ID.Text = "ID";
+            // 
+            // _01Button
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(ID_Val);
+            Controls.Add(ID);
+            Controls.Add(dataGridView1);
+            Controls.Add(button4);
+            Controls.Add(Delete_BTN);
+            Controls.Add(Update_BTN);
+            Controls.Add(sem);
+            Controls.Add(branch);
+            Controls.Add(name);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(Insert_BTN);
+            Name = "_01Button";
+            Text = "_01Button";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button Insert_BTN;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox name;
+        private TextBox branch;
+        private TextBox sem;
+        private Button Update_BTN;
+        private Button Delete_BTN;
+        private Button button4;
+        private DataGridView dataGridView1;
+        private TextBox ID_Val;
+        private Label ID;
+    }
+}
---
+++ _01Button.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace WinFormsApp1
+{
+    public partial class _01Button : Form
+    {
+        SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=G:\\C#\\WinFormsApp1\\Database1.mdf;Integrated Security=True");
+        public _01Button()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string query = "insert into Demo values ('" + name.Text + "','" + branch.Text + "','" + sem.Text + "')";
+            SqlCommand cmd = new SqlCommand(query, con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Inserted Successfully");
+        }
+
+        private void Update_BTN_Click(object sender, EventArgs e)
+        {
+            string query = "update Demo set name='"+name.Text+"',branch='"+branch.Text+"',sem='"+Convert.ToInt32(sem.Text)+"' where id='"+Convert.ToInt32(ID_Val.Text)+"' ";
+            SqlCommand cmd = new SqlCommand(query, con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Updated Successfully");
+        }
+
+        private void Delete_BTN_Click(object sender, EventArgs e)
+        {
+            string query = "delete from Demo where id='" + Convert.ToInt32(ID_Val.Text) + "'";
+            SqlCommand cmd = new SqlCommand(query, con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Deleted Successfully");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            string query = "select * from Demo";
+            SqlDataAdapter sda = new SqlDataAdapter(query, con);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+    }
+}
